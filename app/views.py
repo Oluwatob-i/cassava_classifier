338fe0e4e376993e48359dcbf37aaff06328f5ef
Ubuntu <ubuntu@ip-172-31-30-154.us-east-2.compute.internal>
2021-04-15 15:56:31 +0000

    updated

--- app/views.py
+++ app/views.py
@@ -8,8 +8,10 @@ from django.http import JsonResponse
 
 from fastai.vision.all import *
 
-# upload to s3
+import boto3
+
 s3 = boto3.resource('s3')
+
 
 # Create your views here.
 
@@ -21,9 +23,12 @@ def home(request):
 def get_image(request):
   
     image = Image.open(BytesIO(request.body))
-  
+    
     pred = inf.predict(np.asarray(image))
-    print(pred)
+    
+    con = float((pred[2].max()))
+    confidence = (f"{con:.0%}")
+    print(confidence)
     opts = {
         "0": "Cassava Bacterial Blight (CBB)",
         "1": "Cassava Brown Streak Disease (CBSD)",
@@ -31,6 +36,6 @@ def get_image(request):
         "3": "Cassava Mosaic Disease (CMD)",
         "4": "Healthy",
     }
-    return JsonResponse(opts[pred[0][0]],safe=False)
+    return JsonResponse(opts[pred[0][0]] + ' ' + confidence ,safe=False)
 
 
